7b28be377b10cfc528204b18f0ac9443be655ce7
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-08 11:10:50 +0530

    Simple Array

--- simple_array.py
+++ simple_array.py
@@ -1,9 +1,20 @@
+# input_arr = int(input())
+# number = list(map(int, input().split()))
+# # print(number)
+# sum = 0
+# for i in number:
+#     sum += i
+#     # print(sum,end=" ")
+# print(sum)
+
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+
 input_arr = int(input())
 number = list(map(int, input().split()))
-# print(number)
-sum = 0
-for i in number:
-    sum += i
-    # print(sum,end=" ")
-print(sum)
+result = simpleArraySum(number)
 
+print(result)
