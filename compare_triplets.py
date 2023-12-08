082dc29c40afeae9d6537d7c546ffa1b77f2d110
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-08 12:43:01 +0530

    Compare Triplets

---
+++ compare_triplets.py
@@ -0,0 +1,19 @@
+def compareTriplets(a,b):
+    Alice = 0
+    Bob = 0
+
+    for i in range(0,3):
+        if a[i] > b[i]:
+            Alice += 1
+        elif a[i]<b[i]:
+            Bob += 1
+        else:
+            Alice += 0
+            Bob += 0
+    return (Alice,Bob)
+    
+alice = list(map(int,input().split()))
+bob = list(map(int, input().split()))
+
+result = compareTriplets(alice,bob)
+print(result)
