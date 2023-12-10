85651251769a9cd6a2d61db091fa9a61171dd833
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-10 10:17:30 +0530

    Diagonal Difference

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,21 +1,21 @@
 def diagonalDifference(arr):
-
     add = 0
     sub = 0
-    for i in range(0,len(arr)):
+
+    for i in range(len(arr)):
         add = add + arr[i][i]
-        # print(add)
 
-    for j in range(0,len(arr)):
+    for j in range(len(arr)):
         sub = sub + arr[j][len(arr)-1-j]
-        # print(sub)
+    
+    return add-sub    
 
-    return add-sub
+input1=int(input())
 
-input1 = int(input())
 arr=[]
 for i in range(input1):
     arr.append(list(map(int,input().split())))
 
-result=diagonalDifference(arr)
+# print(arr)
+result = diagonalDifference(arr)
 print(abs(result))
