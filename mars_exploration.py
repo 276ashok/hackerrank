79bc6dff73a3822babda5cbcd80648cf054b0919
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-17 12:00:35 +0530

    Mars Exploration

---
+++ mars_exploration.py
@@ -0,0 +1,18 @@
+def marsExploration(s):
+    count = 0
+
+    for i in range(len(s)//3):
+        if s[i*3]!="S":
+            count +=1
+        
+        if s[i*3+1]!="O":
+            count +=1
+
+        if s [i*3+2]!="S":
+            count +=1
+
+    return count
+
+s = input()
+result = marsExploration(s)
+print(result) 
