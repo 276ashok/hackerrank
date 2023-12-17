ce219c6b07935679f23f5de6fae1a8b069f70dcb
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-17 11:05:20 +0530

    Strong Password

--- strong_password.py
+++ strong_password.py
@@ -1,26 +1,30 @@
-def strongpassword(s):
-    upper = "QWERTYUIOPLKJHGFDSAZXCVBNM"
-    lower = "qwertyuioplkjhgfdsazxcvbnm"
-    number = 1234567890
-    spec = "!@#$%^&*()_+=-"
+number = "0123456789"
+upper = "QWERTYUIOPLKJHGFDSAZXCVBNM"
+lower = "qwertyuioplkjhgfdsazxcvbnm"
+spec = "!@#$%^&*()_+=-"
+
+def strong_password(n, password):
 
     count = 0
 
-    if any(i in upper for i in s) == False:
+    if not any(i in number for i in password):
         count += 1
 
-    if any(i in lower for i in s) == False:
+    if not any(i in upper for i in password):
         count += 1
 
-    if any(i in number for i in s) == False:
+    if not any(i in lower for i in password):
         count += 1
 
-    if any(i in spec for i in s) == False:
+    if not any(i in spec for i in password):
         count += 1
 
-    return max(count, 6-s)
+    if len(password) < 6:
+        return max(count, 6-len(password))
+    
+    return count
 
-
-input = input()
-result = strongpassword(input)
+n = int(input())
+password = input()
+result = strong_password(n, password)
 print(result)
