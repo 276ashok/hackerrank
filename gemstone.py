c606d3042d13502a8351d168ab1c0d1474f304bd
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-18 17:15:37 +0530

    Gemstone

---
+++ gemstone.py
@@ -0,0 +1,21 @@
+def gemstone(arr):
+
+    uni_char = list(set(''.join(arr)))
+    count = len(uni_char)
+
+    for c in uni_char:
+        for j in arr:
+            if c not in j:
+                count -= 1
+                break
+
+    return count
+
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr_item = input()
+    arr.append(arr_item)
+    result = gemstone(arr)
+    print(result)
