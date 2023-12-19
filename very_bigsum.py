3b12120fe4369ff4a59f2bba4950b25d03254205
ASHOK KUMAR M <276ashokkumar@gmail.com>
2023-12-19 12:05:24 +0530

    A Very Bigsum

--- very_bigsum.py
+++ very_bigsum.py
@@ -11,3 +11,12 @@ input2=list(map(int,input().split()))
 result=aVeryBigSum(input2)
 
 print(result)
+
+"""
+sample input
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+
+sample output
+5000000015
+"""
